98eca7f648fbf0825166faee1d54dfacddc08a75
F10RE <windofchange.tour@gmail.com>
2020-06-24 19:44:52 +0300

    initial commit

--- cmds/coin.js
+++ cmds/coin.js
@@ -3,14 +3,12 @@ const fs = require("fs");
 module.exports.run = async (bot, message ,args) => {
     message.channel.send('Монета подбрасывается...')
     
-    var random = Math.floor(Math.random() * 4); // Объявление переменной random - она вычисляет случайное число от 1 до 3
+    var random = Math.floor(Math.random() * 3); // Объявление переменной random - она вычисляет случайное число от 1 до 3
     
     if (random == 1) { // Если вычислено число 1, то выпадает орёл.
         message.channel.send(':full_moon: Орёл!')
     } else if (random == 2) { // Если вычислено число 2, то выпадает решка.
         message.channel.send(':new_moon: Решка!')
-    } else if (random == 3) { // Если вычислено число 3, то монета падает ребром.
-        message.channel.send(':last_quarter_moon: Монета упала ребром!')
     }
 };
     module.exports.help = {
--- cmds/warn.js
+++ cmds/warn.js
@@ -8,24 +8,25 @@ module.exports.run = async (bot, message,args) => {
     }    
      
 
+    if(!args[0]) return send("Вы не указали пользователя!");
+
     if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("У вас нет прав");
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]));
 
-    if(!args[0]) return send("Вы не указали пользователя!");
     if(!rUser) return send("Пользователь не найден")
     
     profile[rUser.id].warns++;
     fs.writeFile('./profile.json',JSON.stringify(profile),(err)=>{
         if(err) console.log(err);
     });
-    if(profile[rUser.id].warns >=3){
-        message.guild.member(rUser).kick("3/3 Предупреждений");
+    if(profile[rUser.id].warns >=30){
+        message.guild.member(rUser).kick("30/30 Предупреждений");
     }
-    let embed = new Discord.RichEmbed()
-    .setDescriptions("Предупреждение!")
+    let embed = new Discord.MessageEmbed()
+    .setDescription("Предупреждение!")
     .addField("Администратор",message.author.username)
-    .addField("Выдал предупреждение",`${rUser.user.usaername}`)
-    .addField("Кол-во предупреждений",`${profile[rUser.id].warns}/3`)
+    .addField("Выдал предупреждение",`${rUser.user.username}`)
+    .addField("Кол-во предупреждений",`${profile[rUser.id].warns}/30`)
 
     message.channel.send(embed);
     }catch(err){
