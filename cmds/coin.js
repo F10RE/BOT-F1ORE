97b17a6de0f6bda63f67d84651c888bb97c0424d
F10RE <windofchange.tour@gmail.com>
2020-06-24 20:51:05 +0300

    монетка

--- cmds/coin.js
+++ cmds/coin.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 module.exports.run = async (bot, message ,args) => {
     message.channel.send('Монета подбрасывается...')
     
-    var random = Math.floor(Math.random() * 3); // Объявление переменной random - она вычисляет случайное число от 1 до 3
+    var random = Math.floor(Math.random() * 2); // Объявление переменной random - она вычисляет случайное число от 1 до 3
     
-    if (random == 1) { // Если вычислено число 1, то выпадает орёл.
+    if (random == 0) { // Если вычислено число 1, то выпадает орёл.
         message.channel.send(':full_moon: Орёл!')
-    } else if (random == 2) { // Если вычислено число 2, то выпадает решка.
+    } else if (random == 1) { // Если вычислено число 2, то выпадает решка.
         message.channel.send(':new_moon: Решка!')
     }
 };
