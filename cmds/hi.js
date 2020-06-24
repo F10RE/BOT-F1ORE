7003e4985b1801087a1c341e944e06ed47bb6d54
Kriper1111 <31844440+Kriper1111@users.noreply.github.com>
2020-06-24 19:12:08 +0300

    re: reverted args, fixed hi

--- cmds/hi.js
+++ cmds/hi.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot, message, args) => {
-    message.channel.send(`Приветик, ${args.user.username}!))`);
+    message.channel.send(`Приветик, ${message.author.username}!))`);
 };
 module.exports.help = {
     names: ["привет", "hi"],
