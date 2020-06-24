f3fe80307af577874c65d660cd1135e9ff81c7a2
F10RE <windofchange.tour@gmail.com>
2020-06-24 21:09:56 +0300

    шифт альт ф

--- cmds/coin.js
+++ cmds/coin.js
@@ -1,16 +1,16 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
-module.exports.run = async (bot, message ,args) => {
+module.exports.run = async (bot, message, args) => {
     message.channel.send('Монета подбрасывается...')
-    
-    var random = Math.floor(Math.random() * 2); // Объявление переменной random - она вычисляет случайное число от 1 до 3
-    
-    if (random == 0) { // Если вычислено число 1, то выпадает орёл.
+
+    var random = Math.floor(Math.random() * 2); // Объявление переменной random - она вычисляет случайное число от 0 до 1
+
+    if (random == 0) { // Если вычислено число 0, то выпадает орёл.
         message.channel.send(':full_moon: Орёл!')
-    } else if (random == 1) { // Если вычислено число 2, то выпадает решка.
+    } else if (random == 1) { // Если вычислено число 1, то выпадает решка.
         message.channel.send(':new_moon: Решка!')
     }
 };
-    module.exports.help = {
-        names: ["монетка"]
-    };
+module.exports.help = {
+    names: ["монетка"]
+};А
--- cmds/warn.js
+++ cmds/warn.js
@@ -1,39 +1,39 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 let profile = require("../profile.json");
-module.exports.run = async (bot, message,args) => {
-    try{
-    function send (msg){
-        message.channel.send(msg);
-    }    
-     
+module.exports.run = async (bot, message, args) => {
+    try {
+        function send(msg) {
+            message.channel.send(msg);
+        }
 
-    if(!args[0]) return send("Вы не указали пользователя!");
 
-    console.log(message.member.permissions.toArray());
+        if (!args[0]) return send("Вы не указали пользователя!");
 
-    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(`${message.author.username}, у вас не хватает прав.`);
-    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]));
+        console.log(message.member.permissions.toArray());
 
-    if(!rUser) return send("Пользователь не найден.")
-    
-    profile[rUser.id].warns++;
-    fs.writeFile('./profile.json',JSON.stringify(profile),(err)=>{
-        if(err) console.log(err);
-    });
-    if(profile[rUser.id].warns >=30){
-        message.guild.member(rUser).kick("30/30 Предупреждений.");
+        if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(`${message.author.username}, у вас не хватает прав.`);
+        let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]));
+
+        if (!rUser) return send("Пользователь не найден.")
+
+        profile[rUser.id].warns++;
+        fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
+            if (err) console.log(err);
+        });
+        if (profile[rUser.id].warns >= 30) {
+            message.guild.member(rUser).kick("30/30 Предупреждений.");
+        }
+        let embed = new Discord.MessageEmbed()
+            .setDescription("Предупреждение!")
+            .addField("Администратор", message.author.username)
+            .addField("Выдал предупреждение", `${rUser.user.username}`)
+            .addField("Кол-во предупреждений", `${profile[rUser.id].warns}/30`)
+
+        message.channel.send(embed);
+    } catch (err) {
+        console.log(`1.${err.name}\n2.${err.stack}`);
     }
-    let embed = new Discord.MessageEmbed()
-    .setDescription("Предупреждение!")
-    .addField("Администратор",message.author.username)
-    .addField("Выдал предупреждение",`${rUser.user.username}`)
-    .addField("Кол-во предупреждений",`${profile[rUser.id].warns}/30`)
-
-    message.channel.send(embed);
-    }catch(err){
-    console.log(`1.${err.name}\n2.${err.stack}`);
-    }    
 
 };
 module.exports.help = {
