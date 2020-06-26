e1b01d1fcc02f6377666b8171dd2a9834bb906a8
Kriper1111 <31844440+Kriper1111@users.noreply.github.com>
2020-06-26 21:54:28 +0300

    moved to moment, dfs

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,6 +1,15 @@
+const {Client, Message} = require('discord.js') // чтобы @param-ам было легче
 const {MessageEmbed} = require('discord.js') //Для создания встроенного сообщения      //Крипер, читай комменты, специально для тебя весь код и комменты писал
-const strftime = require('strftime') //Подключение strftime к файлу                    //Если ты читаешь это не с моего "тыка" - проверь пожалуйста код, мало-ли я накосячил где-то
+// const strftime = require('strftime') //Подключение strftime к файлу                    //Если ты читаешь это не с моего "тыка" - проверь пожалуйста код, мало-ли я накосячил где-то
+const moment = require('moment'); // юзаем moment -- он может больше. В РАЗЫ
 
+// я чаще всего понимаю как работает код. лучше комментируй какие-либо "нагромождения".
+// ну или так, мне не сильно принципиально
+
+/**
+ * @param {Client} bot
+ * @param {Message} message
+ */
 module.exports.run = async (bot, message, args) => {
     let member = message.guild.member(message.mentions.users.first() || message.guild.members.fetch(args[0]))
     let argsUser
@@ -20,23 +29,25 @@ module.exports.run = async (bot, message, args) => {
     else if (argsUser.presence.game.type == 2) game = `Слушает **${argsUser.presence.game.name}**` //Если участник имеет статус "Слушает ..."
     else if (argsUser.presence.game.type == 3) game = `Смотрит **${argsUser.presence.game.name}**` //Если участник имеет статус "Смотрит ..."
 
-    let day = 1000 * 60 * 60 * 24   //Один день = 86 400 000 миллисикунд (если не знал)
-    let date1 = new Date(message.createdTimestamp) //Дата отправки командного сообщения
-    let date2 = new Date(argsUser.createdTimestamp) //Дата регистрации аккаунта
-    let date3 = new Date(message.guild.member(argsUser).joinedTimestamp) //Дата вступления на сервер
-    let diff1 = Math.round(Math.abs((date1.getTime() - date2.getTime()) / day)) //Кол-во прошедших дней с момента регистрации
-    let diff2 = Math.round(Math.abs((date1.getTime() - date3.getTime()) / day)) //Кол-во прошедших дней с момента всупления на сервер
+    moment.locale('ru')
+    let registerUptime = moment(message.createdTimestamp).to(argsUser.createdTimestamp)
+    let joinUptime = moment(message.createdTimestamp).to(message.guild.member(argsUser).joinedTimestamp)
+    // moment() --> создаёт объект момента из текущего(почти) времени, с которым мы работаем.
+    // moment().to() --> текстовая разница между сейчас и точкой в прошлом. moment.from() -- наоборот, между сейчас и точкой в будущем
+    // moment().locale() --> ставит язык на русский
+    // moment().format() --> почти как strftime, но немного другие переменные. см больше: https://momentjs.com/docs/#/displaying/format/
 
     let embed = new MessageEmbed()
         .setTitle(argsUser.username)
         .setDescription(game)
-        .addField('Дата регистрации', `${strftime('%d.%m.%Y в %H:%M', new Date(argsUser.createdTimestamp))}\n(${diff1} дн. назад)`, true)
-        .addField('Дата вступления', `${strftime('%d.%m.%Y в %H:%M', new Date(message.guild.member(argsUser).joinedTimestamp))}\n(${diff2} дн. назад)`, true)
+        .addField('Дата регистрации', `${moment(argsUser.createdTimestamp).format('D.MM.YYYY HH:mm')}\n(${registerUptime})`, true)
+        .addField('Дата вступления', `${moment(message.guild.member(argsUser).joinedTimestamp).format('D.MM.YYYY HH:mm')}\n(${joinUptime})`, true)
         .addField('Роли', message.guild.member(argsUser).roles.cache.filter(r => r.id != message.guild.id).map(role => role.name).join(', ') || 'Не имеет')
         .setColor(message.guild.member(argsUser).displayHexColor) //Если у роли нет цвета, то будет отображаться чёрный
         .setTimestamp()
-        .setAuthor(message.author.tag, message.author.avatarURL({ size: 256 }))
+        .setThumbnail(message.author.avatarURL({ size: 256 }))
         .setFooter(`ID: ${argsUser.id}`)
+    // ты был близок, от тебя требовалось увидеть `message.author.avatarURL({ size: 256 })` и скопировать его в .setThumbnail
     await message.channel.send(embed)
 }
 
